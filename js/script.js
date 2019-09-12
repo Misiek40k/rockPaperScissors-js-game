fadb91208a73489eba633571474c98fe376dc79a
Misiek40k <comanderrr@gmail.com>
2019-09-12 20:21:36 +0200

    Add getMoveName and displayResult functions and insert switch to displayResult

--- js/script.js
+++ js/script.js
@@ -2,31 +2,49 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
+let getMoveName = (moveName) => {
+    if (moveName === ROCK) {
+        return 'Rock';
+    } else if (moveName === PAPER) {
+        return 'Paper';
+    } else if (moveName === SCISSORS) {
+        return 'Scissors';
+    } else {
+        printMessage('Uknkown move !')
+    }
+}
+
+let displayResult = (computerMove, playerMove) => {
+    switch (true) {
+        case computerMove === ROCK && playerMove === PAPER:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Win !!`);
+            break;
+        case computerMove === ROCK && playerMove === SCISSORS:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Lose !!`);
+            break;
+        case computerMove === PAPER && playerMove === ROCK:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Lose !!`);
+            break;
+        case computerMove === PAPER && playerMove === SCISSORS:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Win !!`);
+            break;
+        case computerMove === SCISSORS && playerMove === PAPER:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Lose !!`);
+            break;
+        case computerMove === SCISSORS && playerMove === ROCK:
+            printMessage(`Computer move is: ${getMoveName(computerMove)}<br>Your move is: ${getMoveName(playerMove)}<br>You Win !!`);
+            break;
+        case computerMove === playerMove:
+            printMessage('Both moves are same! Draw !!');
+            break;
+        case playerMove !== computerMove:
+            printMessage('Wrong move ! Try again!');
+    }
+}
+
+
 const computerMove = Math.floor(Math.random() * 3 + 1);
 const playerMove = parseInt(prompt('Chose your move! 1: rock, 2: paper, 3: scissors.'));
 
-switch (true) {
-    case computerMove === ROCK && playerMove === PAPER:
-        printMessage(`Computer move is: Rock<br>Your move is: Paper<br>You Win !!`);
-        break;
-    case computerMove === ROCK && playerMove === SCISSORS:
-        printMessage(`Computer move is: Rock<br>Your move is: Scissors<br>You Lose !!`);
-        break;
-    case computerMove === PAPER && playerMove === ROCK:
-        printMessage(`Computer move is: Paper<br>Your move is: Rock<br>You Lose !!`);
-        break;
-    case computerMove === PAPER && playerMove === SCISSORS:
-        printMessage(`Computer move is: Paper<br>Your move is: Scissors<br>You Win !!`);
-        break;
-    case computerMove === SCISSORS && playerMove === PAPER:
-        printMessage(`Computer move is: Scissors<br>Your move is: Paper<br>You Lose !!`);
-        break;
-    case computerMove === SCISSORS && playerMove === ROCK:
-        printMessage(`Computer move is: Scissors<br>Your move is: Rock<br>You Win !!`);
-        break;
-    case computerMove === playerMove:
-        printMessage('Both moves are same! Draw !!');
-        break;
-    case playerMove !== computerMove:
-        printMessage('Wrong move ! Try again!');
-}
+displayResult(computerMove, playerMove);
+
