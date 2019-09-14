7b4238d8f4d16f9f3e9e640efd99d42e407beecc
Misiek40k <comanderrr@gmail.com>
2019-09-14 20:48:24 +0200

    Add buttons and btn listeners and playGame function

--- js/script.js
+++ js/script.js
@@ -2,50 +2,72 @@ const ROCK = 1;
 const PAPER = 2;
 const SCISSORS = 3;
 
-let getMoveName = (moveName) => {
-    if (moveName === ROCK) {
-        return 'Rock';
-    } else if (moveName === PAPER) {
-        return 'Paper';
-    } else if (moveName === SCISSORS) {
-        return 'Scissors';
-    } else {
-        printMessage('Uknkown player move !')
+const btnRock = document.getElementById('btnRock');
+const btnPaper = document.getElementById('btnPaper');
+const btnScissors = document.getElementById('btnScissors');
+
+let playGame = (playerMove) => {
+    let getMoveName = (moveName) => {
+        if (moveName === ROCK) {
+            return 'Rock';
+        } else if (moveName === PAPER) {
+            return 'Paper';
+        } else if (moveName === SCISSORS) {
+            return 'Scissors';
+        } else {
+            printMessage('Uknkown player move !')
+        }
     }
+
+    let displayResult = (computerMove, playerMove) => {
+        let player = getMoveName(playerMove);
+        let computer = getMoveName(computerMove);
+        switch (true) {
+            case computerMove === ROCK && playerMove === PAPER:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
+                break;
+            case computerMove === ROCK && playerMove === SCISSORS:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
+                break;
+            case computerMove === PAPER && playerMove === ROCK:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
+                break;
+            case computerMove === PAPER && playerMove === SCISSORS:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
+                break;
+            case computerMove === SCISSORS && playerMove === PAPER:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
+                break;
+            case computerMove === SCISSORS && playerMove === ROCK:
+                printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
+                break;
+            case computerMove === playerMove:
+                printMessage('Both moves are same! Draw !!');
+                break;
+            default:
+                printMessage('Try again!');
+        }
+    }
+
+    const computerMove = Math.floor(Math.random() * 3 + 1);
+
+    displayResult(computerMove, playerMove);
 }
 
-let displayResult = (computerMove, playerMove) => {
-    let player = getMoveName(playerMove);
-    let computer = getMoveName(computerMove);
-    switch (true) {
-        case computerMove === ROCK && playerMove === PAPER:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
-            break;
-        case computerMove === ROCK && playerMove === SCISSORS:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
-            break;
-        case computerMove === PAPER && playerMove === ROCK:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
-            break;
-        case computerMove === PAPER && playerMove === SCISSORS:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
-            break;
-        case computerMove === SCISSORS && playerMove === PAPER:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Lose !!`);
-            break;
-        case computerMove === SCISSORS && playerMove === ROCK:
-            printMessage(`Computer move is: ${computer}<br>Your move is: ${player}<br>You Win !!`);
-            break;
-        case computerMove === playerMove:
-            printMessage('Both moves are same! Draw !!');
-            break;
-        default:
-            printMessage('Try again!');
-    }
-}
+btnRock.addEventListener('click', () => {
+    clearMessages();
+    playGame(ROCK);
+});
 
-const computerMove = Math.floor(Math.random() * 3 + 1);
-const playerMove = parseInt(prompt('Chose your move! 1: rock, 2: paper, 3: scissors.'));
+btnPaper.addEventListener('click', () => {
+    clearMessages();
+    playGame(PAPER);
+});
 
-displayResult(computerMove, playerMove);
+btnScissors.addEventListener('click', () => {
+    clearMessages();
+    playGame(SCISSORS);
+});
+
+
 
